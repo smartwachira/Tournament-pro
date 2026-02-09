2bad2882bb208323083fbb1e6d11f8c03c85c864
smartwachira <150593527+smartwachira@users.noreply.github.com>
2026-02-09 23:31:08 +0300

    feat: initial PERN+AI project structure and DB connection

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import cors from 'cors';
+import { query } from './src/config/db.js';
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Health Check Route
+app.get('/api/health', (req,res)=>{
+    res.status(200).json({
+        status: 'Green', 
+        message: 'Pitch is ready!'});
+});
+
+//Test DB Connection
+app.get('/api/test-db',async(req, res)=>{
+    try {
+        const results = await query('SELECT NOW()');
+        res.json({
+            status: 'Success',
+            time: results.rows[0].now,
+            message: "Database is communicating perfectly!"
+        });
+    } catch (err){
+        console.error("Error",err);
+        res.status(500).json({
+            status: 'Error',
+            message: 'Database connection failed.'
+        });
+
+    }
+})
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log(`🚀 Server sprinting on http://localhost:${PORT}`));
---
+++ server/src/config/db.js
@@ -0,0 +1,31 @@
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+//Use the connection string directly
+const pool = new Pool({
+  user: 'postgres',
+  host: '127.0.0.1', // Using IP instead of 'localhost' to avoid DNS issues
+  database: 'tournament_db',
+  password: 'Iyano502@', // Note: Use the raw password here, not %40
+  port: 5432,
+});
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+// })
+
+//Diagnostic: Log successful connection
+pool.on('connect',()=>{
+    console.log('⚽ Database connected: The pitch is ready.')
+});
+
+pool.on('error', (err)=>{
+    console.error('❌ Unexpected error on idle database client', err);
+    process.exit(-1);
+});
+
+export const query = (text, params) => pool.query(text, params);
+export default pool;
